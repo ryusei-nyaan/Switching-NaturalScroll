fc7e8a4837e2548000aaee90d60711d1d0ce0673
toratti <70882861+ryusei-nyaan@users.noreply.github.com>
2023-11-18 14:46:26 +0900

    追記
    logging追記

--- mousecontroller.py
+++ mousecontroller.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python
 import subprocess
-
+import logging
 
 
 command = "system_profiler SPUSBDataType"
 result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+
+logging.config.dictConfig({
+    'version': 1,
+    'formatters': {
+        'Formatter':{
+            'format': '%(asctime)s %(name)-10s %(levelname)-8s %(message)s'}
+    },
+    'handlers': {
+        'testHandler': {
+            'class': 'logging.FileHandler',
+            'formatter': 'Formatter',
+            'level': "INFO",
+            'filename': 'test.log',
+        }
+    },
+    'root': {
+        'handlers': ['testHandler'],
+        'level': "WARNING"
+    },
+    'loggers': {
+        'Logger': {
+            'handlers': ['testHandler'],
+            'level': "INFO",
+            'propagate': 0
+        }
+    }
+})
+Logger = logging.getLogger("Logger")
 
 if result.stderr == "":
     
@@ -17,8 +45,10 @@ if result.stderr == "":
         
         if int(output2) != 0:
             command3 = "defaults write -g com.apple.swipescrolldirection -bool false"
+            Logger.info("Natrual scroll off")
             subprocess.run(command3,shell=True)
             command4 = "sudo pkill loginwindow"
+            Logger.info("Log off")
             subprocess.run(command4,shell=True)    
         else:
             exit()
@@ -30,14 +60,16 @@ if result.stderr == "":
 
         if int(output3) != 1:
             command6 = "defaults write -g com.apple.swipescrolldirection -bool true"
+            Logger.info("Natrual scroll on")
             subprocess.run(command6,shell=True)
             command7 = "sudo pkill loginwindow"
+            Logger.info("Log off")
             subprocess.run(command7,shell=True) 
         else:
             exit()
 
 else:
-    print(result.stderr)
+    Logger.error(result.stderr)
 
 
 
